c674c959565e8b6968025950554348d472dc4a1e
taleb khaddour <taleb.khaddour.20311@gmail.com>
2023-06-04 15:09:54 +0300

    fix gallery

--- my-app/src/components/galery/galery.js
+++ my-app/src/components/galery/galery.js
@@ -1,113 +1,47 @@
-import React, { useEffect } from "react";
-import gallery1 from "../../assets/images/gallery1.jpg";
-import gallery2 from "../../assets/images/gallery2.jpg";
-import gallery3 from "../../assets/images/gallery3.jpg";
-import gallery4 from "../../assets/images/gallery4.jpg";
-import gallery5 from "../../assets/images/gallery5.jpg";
-import gallery6 from "../../assets/images/gallery6.jpg";
-import gallery7 from "../../assets/images/gallery7.jpg";
-import gallery8 from "../../assets/images/gallery8.jpg";
+import React, { useEffect, useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import "./galery.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Galery = () => {
+  const [data, setData] = useState([]);
+  const [categories, setCategories] = useState([]);
 
+  useEffect(() => {
+    getData();
+  }, []);
 
-
-
-
+  const getData = () => {
+    axios
+      .get(`https://idecream-backend.onrender.com/api/subCategory/`)
+      .then((response) => {
+        setData(response.data);
+        setCategories(response.data.category_id.name);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   return (
-    <div className="gallery-section"  id="gallery">
-      {" "}
-      <section
-        className="gallery"
-        id="gallery"
-        
-      >
+    <div className="gallery-section" id="gallery">
+      <section className="gallery" id="gallery">
         <h1 className="heading">Our gallery</h1>
 
         <div className="gallery-container" >
-          <a href={gallery1} className="box">
-            <img src={gallery1} alt="" />
-            <div className="icon">
-              {" "}
-              <i>
-                <FaPlus />
-              </i>{" "}
-            </div>
-          </a>
-
-          <a href={gallery2} className="box">
-            <img src={gallery2} alt="" />
-            <div className="icon">
-              {" "}
-              <i>
-                <FaPlus />
-              </i>{" "}
-            </div>
-          </a>
-
-          <a href={gallery3} className="box">
-            <img src={gallery3} alt="" />
-            <div className="icon">
-              {" "}
-              <i>
-                <FaPlus />
-              </i>{" "}
-            </div>
-          </a>
-
-          <a href={gallery4} className="box">
-            <img src={gallery4} alt="" />
-            <div className="icon">
-              {" "}
-              <i>
-                <FaPlus />
-              </i>{" "}
-            </div>
-          </a>
-
-          <a href={gallery5} className="box">
-            <img src={gallery5} alt="" />
-            <div className="icon">
-              {" "}
-              <i>
-                <FaPlus />
-              </i>{" "}
-            </div>
-          </a>
-
-          <a href={gallery6} className="box">
-            <img src={gallery6} alt="" />
-            <div className="icon">
-              {" "}
-              <i>
-                <FaPlus />
-              </i>{" "}
-            </div>
-          </a>
-
-          <a href={gallery7} className="box">
-            <img src={gallery7} alt="" />
-            <div className="icon">
-              {" "}
-              <i>
-                <FaPlus />
-              </i>{" "}
-            </div>
-          </a>
-
-          <a href={gallery8} className="box">
-            <img src={gallery8} alt="" />
-            <div className="icon">
-              {" "}
-              <i>
-                <FaPlus />
-              </i>{" "}
-            </div>
-          </a>
-        </div>
+        {data.length > 0 &&
+          data.map((item) => (
+            <Link to={`/products/${item._id}`} className="box">
+                <img src={item.image} alt="" />
+                <div className="icon ">
+                  <i>
+                    <FaPlus className="rotate-center" />
+                  </i>
+                </div>
+              </Link>
+          ))}
+          </div>
       </section>
     </div>
   );
